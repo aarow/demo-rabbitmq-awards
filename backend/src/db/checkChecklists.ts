6aee53eeadd819fe8e8ba250255a6fe579d21334
Aaron Neville <aaron@codestuff.dev>
2024-10-16 10:01:40 -0700

    feat(backend): refactor streaming

---
+++ backend/src/db/checkChecklists.ts
@@ -0,0 +1,21 @@
+import { type Award } from "../types/Award";
+import sendMessage from "../utils/send-message";
+
+export default async function checkChecklists(award: Award) {
+  if (isChecklistSubmitted(award)) {
+    // send message to RabbitMQ
+    sendMessage({
+      name: process.env.ADMIN_NAME,
+      email: process.env.ADMIN_EMAIL,
+      message: `Award is missing ACURO or OHRO checklist: ${award.award_number}`,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
+
+export function isChecklistSubmitted(award: Award) {
+  return (
+    award.award_number &&
+    (!award.acuro_checklist_submitted || !award.ohro_checklist_submitted)
+  );
+}
--- backend/src/db/stream-awards.ts
+++ backend/src/db/stream-awards.ts
@@ -1,6 +1,7 @@
 import { Collection, Document } from "mongodb";
 import { type Award } from "../types/Award";
-import sendMessage from "../utils/send-message";
+import checkChecklists from "./checkChecklists";
+import checkREC from "./checkREC";
 
 export default async function streamAwards(collection: Collection<Document>) {
   const changeStream = collection.watch();
@@ -17,28 +18,8 @@ export default async function streamAwards(collection: Collection<Document>) {
         return;
       }
 
-      if (
-        award.award_number &&
-        (!award.acuro_checklist_submitted || !award.ohro_checklist_submitted)
-      ) {
-        // send message to RabbitMQ
-        sendMessage({
-          name: process.env.ADMIN_NAME,
-          email: process.env.ADMIN_EMAIL,
-          message: `Award is missing ACURO or OHRO checklist: ${award.award_number}`,
-          timestamp: new Date().toISOString(),
-        });
-      }
-
-      if (award.award_number && !award.rec_submitted) {
-        // send message to RabbitMQ
-        sendMessage({
-          name: process.env.ADMIN_NAME,
-          email: process.env.ADMIN_EMAIL,
-          message: `Award is missing REC submission: ${award.award_number}`,
-          timestamp: new Date().toISOString(),
-        });
-      }
+      checkChecklists(award);
+      checkREC(award);
     }
   });
 }
